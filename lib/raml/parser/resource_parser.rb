67e8bdfa912d42cd49de0b11cd80d283a8322ef4
Pol Miro <polmiro@gmail.com>
2014-09-30 16:44:51 -0700

    Specs for ResourceParser

--- lib/raml/parser/resource_parser.rb
+++ lib/raml/parser/resource_parser.rb
@@ -15,17 +15,17 @@ module Raml
           case key
           when REGEXP
             memo[:resources] ||= {}
-            memo[:resources][key] = self.class.new(key, value).parse
+            memo[:resources][key] = self.class.new(key, safe_hash(key, value)).parse
           when "displayName", "description", "type"
             memo[key.to_s.underscore.to_sym] = value
           when "uriParameters"
             memo[:uri_parameters] = parse_uri_parameters(key, value)
           when "baseUriParameters"
-            memo[:base_uri_parameters] = parse_base_uri_parameters(value)
+            memo[:base_uri_parameters] = parse_base_uri_parameters(key, value)
           when *Method::METHODS
-            memo[key.to_sym] = MethodParser.new(key, value).parse
+            memo[key.to_sym] = MethodParser.new(key, safe_hash(key, value)).parse
           else
-            raise ParserError.new("Unknown resource option `#{key}`")
+            raise ParserError.new("Unknown option `#{key}` for resource")
           end
           memo
         end
--- spec/raml/parser/resource_parser_spec.rb
+++ spec/raml/parser/resource_parser_spec.rb
@@ -1,11 +1,61 @@
 describe Raml::Parser::ResourceParser do
   let(:root) do
     YAML.load <<-YAML
+      displayName: Jobs
+      type: collection
+      description: A collection of jobs
+      uriParameters:
+        AWSAccessKeyId:
+          description: The AWS Access Key ID of the owner of the bucket who grants an Anonymous user access for a request that satisfies the set of constraints in the Policy.
+          type: string
+        acl:
+          description: Specifies an Amazon S3 access control list. If an invalid access control list is specified, an error is generated.
+          type: string
+      baseUriParameters:
+        subapi:
+          description: The name of the sub api
+          type: string
+      get:
+        description: Get the collection of jobs
+      /{jobId}:
+        description: A specific job, a member of the jobs collection
     YAML
   end
 
-  it "parses ...." do
-    document = Raml::Parser::ResourceParser.new(root).parse
-    expect(false).to eq(true)
+  let(:resource) { Raml::Parser::ResourceParser.new("/jobs", root).parse }
+
+  it "parses the name" do
+    expect(resource.name).to eq("/jobs")
+  end
+
+  it "parses the description" do
+    expect(resource.description).to eq("A collection of jobs")
+  end
+
+  it "parses the type" do
+    expect(resource.type).to eq("collection")
+  end
+
+  it "parses the displayName" do
+    expect(resource.display_name).to eq("Jobs")
+  end
+
+  it "parses the children resources" do
+    expect(resource.resources.count).to eq(1)
+    expect(resource.resources["/{jobId}"].description).to match("A specific job")
+  end
+
+  it "parses the uriParameters" do
+    expect(resource.uri_parameters.count).to eq(2)
+    expect(resource.uri_parameters["acl"].type).to match("string")
+  end
+
+  it "parses the baseUriParameters" do
+    expect(resource.base_uri_parameters.count).to eq(1)
+    expect(resource.base_uri_parameters["subapi"].description).to eq("The name of the sub api")
+  end
+
+  it "parses the methods" do
+    expect(resource.get.description).to eq("Get the collection of jobs")
   end
 end
